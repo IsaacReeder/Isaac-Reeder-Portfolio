bbc6594c1ecbd2979ebc3206fb4c68fb2a03c087
Isaac R <isaacreeder5@gmail.com>
2020-03-12 15:10:31 -0700

    Landing page completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import {
   BrowserRouter as Router,
@@ -8,20 +7,214 @@ import {
   Redirect
 } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <LandingPage />
-        </Route>
-        <Route path="/projects" exact>
-          <ProjectsPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
+import LandingPage from "./components/LandingPage/LandingPage";
+import ProjectsPage from "./components/ProjectsPage/ProjectsPage";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkBackgroundModes: [
+        "day",
+        "terminal",
+        "torquoise",
+        "alizarin",
+        "amythyst",
+        "carrot",
+        "peterriver"
+      ],
+      lightBackgroundModes: [
+        "night",
+        "lightred",
+        "lightpurple",
+        "lightgreen",
+        "lightblue",
+        "lightyellow"
+      ],
+      backgroundType: this.props.backgroundType || "plain",
+      appClass: this.props.plainBackgroundMode || "daylight",
+      devIntro: this.props.devIntro || "Lorem Ipsum",
+      devDesc:
+        this.props.devDesc ||
+        "Aute veniam ut deserunt cillum irure pariatur Lorem dolore anim nostrud quis veniam elit culpa.",
+      backgroundMode: "default",
+      backgroundIndex: 0,
+      bgStyle: {},
+      icons: this.props.icons || []
+    };
+  }
+
+  componentWillMount = () => {
+    if (this.checkIfPlainTypeEnabled()) {
+      return true;
+    } else if (this.checkIfGradientTypeEnabled()) {
+      this.setState({
+        appClass: "gradient",
+        bgStyle: this.prepareGradientStyleSheets()
+      });
+    } else if (this.checkIfImageTypeEnabled()) {
+      this.setState({
+        appClass: "full-bg-image",
+        bgStyle: this.prepareBackgroundImageStyle()
+      });
+    }
+  };
+
+  checkIfNightModeEnabled = () => {
+    return (
+      this.state.backgroundType === "plain" &&
+      this.state.appClass === "nightlight"
+    );
+  };
+
+  checkIfDayModeEnabled = () => {
+    return (
+      this.state.backgroundType === "plain" &&
+      this.state.appClass === "daylight"
+    );
+  };
+
+  checkIfGradientTypeEnabled = () => {
+    return this.state.backgroundType === "gradient";
+  };
+
+  checkIfPlainTypeEnabled = () => {
+    return this.state.backgroundType === "plain";
+  };
+
+  checkIfImageTypeEnabled = () => {
+    return this.state.backgroundType === "image";
+  };
+
+  prepareGradientStyleSheets = () => {
+    if (this.props.gradientColors) {
+      return {
+        background:
+          "linear-gradient(-45deg, " + this.props.gradientColors + ")",
+        backgroundSize: "400% 400%"
+      };
+    } else {
+      return {
+        background:
+          "linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB)",
+        backgroundSize: "400% 400%"
+      };
+    }
+  };
+
+  prepareBackgroundImageStyle = () => {
+    if (this.props.backgroundImageUrl) {
+      return {
+        background:
+          'url("' +
+          this.props.backgroundImageUrl +
+          '") no-repeat center center fixed',
+        backgroundSize: "cover"
+      };
+    } else {
+      return {
+        background:
+          'url("/images/sample-background.jpg") no-repeat center center fixed',
+        backgroundSize: "cover"
+      };
+    }
+  };
+
+  getDefaultModeBasedOnBackgroundType = () => {
+    if (this.checkIfNightModeEnabled()) {
+      return this.state.lightBackgroundModes[0];
+    } else if (this.checkIfDayModeEnabled()) {
+      return this.state.darkBackgroundModes[0];
+    }
+  };
+
+  changeThemeMode = e => {
+    if (this.checkIfNightModeEnabled()) {
+      this.setState({
+        appClass: "daylight",
+        backgroundIndex: 0,
+        backgroundMode: this.state.darkBackgroundModes[0]
+      });
+    } else if (this.checkIfDayModeEnabled()) {
+      this.setState({
+        appClass: "nightlight",
+        backgroundIndex: 0,
+        backgroundMode: this.state.lightBackgroundModes[0]
+      });
+    }
+  };
+
+  changeBackgroundBasedonMode = () => {
+    if (
+      this.checkIfNightModeEnabled() &&
+      this.state.backgroundIndex < this.state.lightBackgroundModes.length - 1
+    ) {
+      this.setState({
+        backgroundIndex: this.state.backgroundIndex + 1,
+        backgroundMode: this.state.lightBackgroundModes[
+          this.state.backgroundIndex + 1
+        ]
+      });
+    } else if (
+      this.checkIfDayModeEnabled() &&
+      this.state.backgroundIndex < this.state.darkBackgroundModes.length - 1
+    ) {
+      this.setState({
+        backgroundIndex: this.state.backgroundIndex + 1,
+        backgroundMode: this.state.darkBackgroundModes[
+          this.state.backgroundIndex + 1
+        ]
+      });
+    } else {
+      this.setState({
+        backgroundIndex: 0,
+        backgroundMode: this.getDefaultModeBasedOnBackgroundType()
+      });
+    }
+  };
+
+  render() {
+    const { appClass, bgStyle, backgroundMode } = this.state;
+
+    return (
+      <div className={appClass} style={bgStyle}>
+        <div className="change-mode" onClick={this.changeThemeMode} />
+        <div
+          className={backgroundMode}
+          onClick={this.changeBackgroundBasedonMode}
+        >
+          <main className="App-main">
+            <Router>
+              <Switch>
+                <Route path="/" exact>
+                  <LandingPage
+                    devIntro="Hi, I'm Isaac Reeder"
+                    plainBackgroundMode="daylight"
+                    devDesc="JavaScript | ReactJS |  MongoDB | API's "
+                    gradientColors="#EE7752, #E73C7E, #23A6D5, #23D5AB"
+                    icons={[
+                      {
+                        image: "fa-github",
+                        url: "https://github.com/IsaacReeder"
+                      },
+                      {
+                        image: "fa-linkedin",
+                        url: "https://www.linkedin.com/in/isaac-reeder/"
+                      }
+                    ]}
+                  />
+                </Route>
+                <Route path="/projects" exact>
+                  <ProjectsPage />
+                </Route>
+                <Redirect to="/" />
+              </Switch>
+            </Router>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Typist from "react-typist";
+import "./LandingPage.scss";
+// import Configs from "./configurations.json";
+
+export default class LandingPage extends Component {
+  render(props) {
+    const {
+      appClass,
+      bgStyle,
+      backgroundMode,
+      devIntro,
+      devDesc,
+      icons
+    } = this.props;
+
+    return (
+      <div className={appClass} style={bgStyle}>
+        <div
+          className={backgroundMode}
+          onClick={this.changeBackgroundBasedonMode}
+        >
+          <main className="App-main">
+            <h1 className="intro">{devIntro}</h1>
+            <div className="tagline">
+              <Typist>{devDesc}</Typist>
+            </div>
+            <div className="icons-social">
+              {icons.map(icon => (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`${icon.url}`}
+                >
+                  <i className={`fab ${icon.image}`} />
+                </a>
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
