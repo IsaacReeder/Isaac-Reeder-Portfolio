d4748975c6e4b74ddf5a8fbd04468feab4c88856
Isaac R <isaacreeder5@gmail.com>
2020-03-13 11:58:09 -0700

    card placement and styling

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -1,21 +1,33 @@
 import React, { Component, Fragment } from "react";
+import "./ProjectsPage.scss";
+import { CSSTransition } from "react-transition-group";
 
 export default class ProjectsPage extends Component {
   render() {
     const { projects } = this.props;
     return (
       <div>
-        <div className="icons-social">
-          {projects.map(project => (
-            <Fragment>
-              {" "}
-              <h1 target="_blank" rel="noopener noreferrer">
-                {`${project.pName}`}
-              </h1>
-              <img alt="pic" src={`${project.image}`}></img>
-            </Fragment>
-          ))}
-        </div>
+        <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
+          <div className="projects">
+            {projects.map(project => (
+              <Fragment>
+                <div className="card">
+                  {" "}
+                  <h1 target="_blank" rel="noopener noreferrer">
+                    {`${project.pName}`}
+                  </h1>
+                  <img
+                    className="image"
+                    alt="pic"
+                    src={`${project.image}`}
+                  ></img>
+                  <h5>{`${project.description}`}</h5>
+                  <h6>{`${project.technologies}`}</h6>
+                </div>
+              </Fragment>
+            ))}
+          </div>
+        </CSSTransition>
       </div>
     );
   }
